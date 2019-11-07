29f4a932b80ae6dd0a21ae4bd5f3edabc4b9d427
Shannon Clarke <shannonclarke@Shannons-MacBook-Pro.local>
2019-11-07 17:35:59 -0400

    Check if user exists after form submission

--- config.php
+++ config.php
@@ -3,21 +3,21 @@
 
     //setting the data constants
     define('DB_USER', 'root');
-    define('DB_NAME', 'vlrms');
+    define('DB_NAME', 'test_uwi');
     define('DB_PASS', '');
     define('DB_HOST', 'localhost');
 
     //connect to database
     $host = 'localhost';
 	$username = 'root';
-	$password = '';
-	$database = 'assignment2';
+	$password = 'root';
+	$database = 'test_uwi';
 	$conn = mysqli_connect($host, $username, $password, $database);
 
 
 	//$conn is outr database object
 	if(!$conn)
 	{
-		die('Cannot connect to server');
+		die('Cannot connect to server 2');
 	}
 ?>
--- sign_in_user.php
+++ sign_in_user.php
@@ -4,21 +4,50 @@
     $error = ''; //Variable to store error message
     $national_Id = $_POST['national_Id'];
     $password = $_POST['pword'];
+
+    //Checking if the form was submitted
     if(isset($_POST['sign_in']))
     {
+        //Check if form fields were submitted empty or not
         if(empty($national_Id) || empty($password))
         {
             $error = "Username or Password is invalid";
-            header("location: index.php");
+            header("location: index.php"); //redirecting to index.php
         }
         else 
         {
+            //Verify if fields contained valid info
+            //Check if nationalID was a number
+            //Verify that the password was a string
+
+            //TIP: Escape all strings 
+
             //Define names and license#
-            $sql = "SELECT * FROM citizen WHERE National_ID ='".$national_Id."' AND Password = '".$password."' limit 1";
+            $sql = "SELECT * FROM citizen WHERE National_ID ='".$national_Id."' AND Password = '".$password."'";
             $result = $conn->query($sql);
 
-            header("location: public_console.html");
-                echo "logged in";
+            // echo '<pre>';
+            // print_r($result);
+            // echo '</pre>';
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                // $rows = array();
+                // while($row = $result->fetch_assoc()) {
+                //     array_push($rows,array('id'=>$row['id'], 'name'=>$row['user_name'], 'text'=>$row['text'], 'date'=>$row['date_created']));
+                // }
+                // print_r($result->fetch_assoc());
+                $row = $result->fetch_assoc();
+                echo '<pre>';
+            print_r($row);
+            echo '</pre>';
+                
+            } else {
+                die('No user matches that nationalid/password combination');
+            }
+
+            // header("location: public_console.html");
+            //     echo "logged in";
         }
     }
 ?>
